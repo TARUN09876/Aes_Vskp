af3b4257a1be29ae88a3c211efc91285bc4359b4
TARUN09876 <tarunsahukar2002@gmail.com>
2024-02-15 10:25:55 +0530

    new readme added -s

---
+++ OJT/Tarun/Workspace/ADV_C/app1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"header1.h"
+
+int main (){
+
+	printf("addition of 2 numbrs: %d", add1(1,2));
+	printf("substraction of 2 numbrs: %d", sub1(1,2));
+ 	printf("multiplication of 2 numbrs: %d", mul1(1,2));
+	printf("division of 2 numbrs: %d", div1(1,2));
+ return 0;
+}
+
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
-happy valentines day 
+This readme file tells about my project file
+
+the file is in  /home/tarun/Work/Aes_Vskp
+
+HAPPY SARASWATI PUJA TO ALL
